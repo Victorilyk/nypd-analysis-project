a7624fd4ef4f7ef4062693291e96a6256e296a2d
Victorilyk <v.ilyk@student.uw.edu.pl>
2025-06-22 20:19:11 +0300

    reporter.py

--- analysis_lib/reporter.py
+++ analysis_lib/reporter.py
@@ -1,5 +1,13 @@
-def save_report(df, output_path):
-    print("Saving report to:", output_path)
-    print("Data to be saved:")
-    print(df.head())
-    df.to_csv(output_path, sep=';', index=False, encoding='utf-8-sig')
+import pandas as pd
+
+def save_report(df: pd.DataFrame, filename: str):
+    name = filename.replace('.csv', '')
+
+    if isinstance(df, pd.DataFrame):
+        print(f"{name.upper()}: {list(df.columns)}")
+    elif isinstance(df, pd.Series):
+        print(f"{name.upper()}: Series with name: {df.name}")
+    else:
+        print(f"{name.upper()}: Unknown type: {type(df)}")
+
+    df.to_csv(filename, index=False)
