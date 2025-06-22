a064cc369e2793a98a0ee0e0a5f95930bde2d1d7
Victorilyk <v.ilyk@student.uw.edu.pl>
2025-06-22 16:58:34 +0300

    reporter.py

--- analysis_lib/reporter.py
+++ analysis_lib/reporter.py
@@ -1,5 +1,5 @@
 def save_report(df, output_path):
+    print("Saving report to:", output_path)
+    print("Data to be saved:")
+    print(df.head())
     df.to_csv(output_path, index=False)
-print("Saving report to:", path)
-print("Data to be saved:")
-print(df.head())
