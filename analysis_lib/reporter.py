1a4f1a3b7760013a695e8575ef2583b2c114e101
Victorilyk <v.ilyk@student.uw.edu.pl>
2025-06-22 19:09:01 +0300

    reporter.py

--- analysis_lib/reporter.py
+++ analysis_lib/reporter.py
@@ -2,4 +2,4 @@ def save_report(df, output_path):
     print("Saving report to:", output_path)
     print("Data to be saved:")
     print(df.head())
-    df.to_csv(output_path, index=False, encoding='utf-8-sig')  # encoding for Excel compatibility
+    df.to_csv(output_path, sep=';', index=False, encoding='utf-8-sig')
